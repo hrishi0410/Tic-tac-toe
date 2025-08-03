b976872056636328cf21834005a6feae66b1e218
Rushikesh <rushikeshsa2010@gmail.com>
2025-08-03 14:39:14 +0530

    tic-tac-toe

---
+++ App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import TicTacToe from './components/TicTacToe/TicTacToe';
+
+function App() {
+  return (
+    <div >
+      <TicTacToe/>
+    </div>
+  );
+}
+
+export default App;
+
+
+//run this code from Desktop\React Projects\tic-tac-toe\ttt path with npm start cmd
+//TicTacToe is in src/components/TicTacToe/TicTacToe.jsx
